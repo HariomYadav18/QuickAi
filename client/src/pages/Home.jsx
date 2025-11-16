464ed41cc4a9461d69eaa2435fdbe9afb48a5591
HariomYadav18 <hariomchelsea@gmail.com>
2025-11-16 22:53:43 +0530

    Home page - navigation bar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import AiTools from "../components/AiTools";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Plan from "../components/Plan";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <Hero />
+      <AiTools />
+      <Testimonial />
+      <Plan />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;

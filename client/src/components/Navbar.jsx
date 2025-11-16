ce1bf4af6181755aedc317f621fe1e7f22783416
HariomYadav18 <hariomchelsea@gmail.com>
2025-11-16 23:13:26 +0530

    Hero section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { ArrowRight } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const navigate = useNavigate();

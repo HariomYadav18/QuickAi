7c320d81aaf65e3c3e6be1a39199c3cc11da2629
HariomYadav18 <hariomchelsea@gmail.com>
2025-11-19 08:41:54 +0530

    modified:   client/src/App.jsx 	modified:   client/src/components/AiTools.jsx 	modified:   client/src/components/CreationItem.jsx 	modified:   client/src/components/Footer.jsx 	modified:   client/src/components/Hero.jsx 	modified:   client/src/components/Navbar.jsx 	modified:   client/src/components/Plan.jsx 	modified:   client/src/components/Sidebar.jsx 	modified:   client/src/main.jsx 	modified:   client/src/pages/BlogTitles.jsx 	modified:   client/src/pages/Community.jsx 	modified:   client/src/pages/Dashboard.jsx 	modified:   client/src/pages/GenerateImages.jsx 	modified:   client/src/pages/Home.jsx 	modified:   client/src/pages/Layout.jsx 	modified:   client/src/pages/RemoveBackground.jsx 	modified:   client/src/pages/RemoveObject.jsx 	modified:   client/src/pages/ReviewResume.jsx 	modified:   client/src/pages/WriteArticle.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,22 @@
-import AiTools from "../components/AiTools";
-import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
-import Testimonial from "../components/Testimonial";
-import Plan from "../components/Plan";
-import Footer from "../components/Footer";
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
+import Plan from '../components/Plan'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
-      <AiTools />
+      <AiTools/>
       <Testimonial />
-      <Plan />
-      <Footer />
+      <Plan/>
+      <Footer/>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
